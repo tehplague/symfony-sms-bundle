4440675c35d34d34e16aff2617755eda65bf4278
Christian Spoo <christian.spoo@jtl-software.com>
2013-12-02 08:37:31 +0100

    Add public API for querying account balance

--- Services/SmsFactory.php
+++ Services/SmsFactory.php
@@ -12,6 +12,16 @@ class SmsFactory extends ContainerAware
 	private $transport_name = null;
     private $transport = null;
 
+    public function queryBalance()
+    {
+        $this->loadTransport();
+
+        if (!($this->transport instanceof Transport\SmsPrepaidTransportInterface))
+            throw new \Exception('The current transport does not implement SmsPrepaidTransportInterface');
+
+        return $this->transport->queryBalance();
+    }
+
     public function sendSms(Sms $sms)
     {
         $this->loadTransport();
